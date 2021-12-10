882a23df5001ca917c9d36d5db5c8ba7d3d1f967
Le Thi Tuong Vi <5951071119@st.utc2.edu.vn>
2021-12-10 20:38:40 +0700

    Vi update

--- WebsiteDatVe/WebsiteDatVe/Areas/Admin/Controllers/TrangChuController.cs
+++ WebsiteDatVe/WebsiteDatVe/Areas/Admin/Controllers/TrangChuController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteDatVe.Models;
 
 namespace WebsiteDatVe.Areas.Admin.Controllers
 {
     public class TrangChuController : Controller
     {
+        DatVeDB db = new DatVeDB();
         // GET: Admin/Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        //Quản lý máy bay
+        //GET: Admin/Plane
+        public ActionResult Plane()
+        {
+            return View(db.MayBays);
         }
     }
 }
